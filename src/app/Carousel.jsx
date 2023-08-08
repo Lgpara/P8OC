4962e6b50af2e1ddae933f5baa15ec63d19327a2
Louis Gerber <louisgerber004@gmail.com>
2023-08-08 13:10:02 +0200

    nth

--- src/app/Carousel.jsx
+++ src/app/Carousel.jsx
@@ -2,19 +2,28 @@ import leftArrow from '../content/images/left_arrow.svg';
 import rightArrow from '../content/images/right_arrow.svg';
 import data from "../content/data.json"
 import "../styles/carousel.css"
-import { carouselPos, carouselScript } from './carouselFunction';
+import React, { useState } from 'react';
 
-let carouselImgs = data[0].pictures[carouselPos]
-let carouselLength = data[0].pictures.length
+// let carouselImgs = data[0].pictures[carouselPos]
+// let carouselLength = data[0].pictures.length
 
-
+let carouselPos = 0
 
 
 function Carousel () {
-    
-
-
-
+    const [imgValue, setImgValue] = useState(data[0].pictures[carouselPos])
+    let carouselImgs = useState(data[0].pictures[carouselPos])
+    let carouselLength = data[0].pictures.length
+    function carouselScript(variable){
+        carouselPos = carouselPos + variable
+        if(carouselPos > carouselLength - 1){
+            carouselPos = 0
+        }
+        if(carouselPos < 0){
+            carouselPos = carouselLength - 1
+        }
+        console.log(carouselPos)
+    }
     return(
         <div className="carouselContainer">
             <div className="imgContainer">
--- src/app/carouselFunction.js
+++ src/app/carouselFunction.js
@@ -1,18 +1,18 @@
-import data from "../content/data.json"
-let carouselLength = data[0].pictures.length
+// import data from "../content/data.json"
+// let carouselLength = data[0].pictures.length
 
 
 
-export let carouselPos = 1
+// export let carouselPos = 1
 
 
-export function carouselScript (variable){
-    carouselPos = carouselPos + variable
-    if(carouselPos > carouselLength - 1){
-        carouselPos = 0
-    }
-    if(carouselPos < 0){
-        carouselPos = carouselLength - 1
-    }
-    console.log(carouselPos)
-}
+// export function carouselScript (variable){
+//         carouselPos = carouselPos + variable
+//         if(carouselPos > carouselLength - 1){
+//             carouselPos = 0
+//         }
+//         if(carouselPos < 0){
+//             carouselPos = carouselLength - 1
+//         }
+//         console.log(carouselPos)
+// }
