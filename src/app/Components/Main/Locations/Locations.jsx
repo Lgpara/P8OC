6b2ee38ecb4442e72dffdce2cd42eb040a33cc93
Louis Gerber <louisgerber004@gmail.com>
2023-08-16 15:35:23 +0200

    cleaned version

--- src/app/Components/Main/Locations/Locations.jsx
+++ src/app/Components/Main/Locations/Locations.jsx
@@ -1,26 +1,16 @@
 import data from "./data.json"
 import "./styles/locations.css"
 import { Link } from "react-router-dom"
-import React, {useState} from "react"
 
 function Location () {
-
-    const [selectedAppartement, setSelectedAppartement] = useState(null)
-
-    const handleClick = (appartementId) => {
-        const appartementIndex = data.findIndex(location => location.id === appartementId)
-        setSelectedAppartement(appartementIndex)
-        console.log(appartementIndex) 
-    }
-
 
     return (
         <div className="locCardContainer">
             {data.map(location => {
                 return (
-                    <div className="locCard">
-                        <Link onClick={() => handleClick(location.id)} to={`/fiche-logement/${location.id}`} className="imgContainer">
-                            <img key={location.id} src={location.cover} alt={location.title} />
+                    <div key={location.id} className="locCard">
+                        <Link to={`/fiche-logement/${location.id}`} className="imgContainer">
+                            <img src={location.cover} alt={location.title} />
                         </Link>
                         <p>{location.title}</p>
                     </div>
